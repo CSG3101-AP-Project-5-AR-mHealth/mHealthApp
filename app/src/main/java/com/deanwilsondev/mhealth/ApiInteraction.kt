0b8fddeca804a9c49b2a31c61996fbab150b1c8d
Jarrad <jmerric0@our.ecu.edu.au>
2021-09-28 22:48:16 +0800

    rework api interaction into its own class

---
+++ app/src/main/java/com/deanwilsondev/mhealth/ApiInteraction.kt
@@ -0,0 +1,48 @@
+package com.deanwilsondev.mhealth
+
+import java.io.OutputStreamWriter
+import java.net.URL
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
+class ApiInteraction {
+    companion object {
+        var Ip: String = "127.0.0.1"
+        var Port: String = "8000"
+
+        fun sendTokenToApi(token: String) {
+            val mURL = URL("https://$Ip:$Port/registration/")
+            var reqParam = "{ \"token\": \"$token\"}"
+
+            with((mURL.openConnection() as HttpsURLConnection).apply{
+                sslSocketFactory = createSocketFactory(listOf("TLSv1.2"))
+                hostnameVerifier = HostnameVerifier { _, _ -> true }
+                readTimeout = 2_000
+                connectTimeout = 1_000
+            }){
+                // optional default is GET
+                requestMethod = "POST"
+
+                val wr = OutputStreamWriter(outputStream);
+                wr.write(reqParam);
+                wr.flush();
+            }
+        }
+
+        // trusts every cert as we're self-signing the django https server
+        private fun createSocketFactory(protocols: List<String>) =
+            SSLContext.getInstance(protocols[0]).apply {
+                val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+                    override fun checkClientTrusted(certs: Array<X509Certificate>, authType: String) = Unit
+                    override fun checkServerTrusted(certs: Array<X509Certificate>, authType: String) = Unit
+                })
+                init(null, trustAllCerts, SecureRandom())
+            }.socketFactory
+    }
+}
--- app/src/main/java/com/deanwilsondev/mhealth/MainActivity.kt
+++ app/src/main/java/com/deanwilsondev/mhealth/MainActivity.kt
@@ -18,10 +18,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sendTokenToApi()
+        doFcmTokenRegistration()
     }
 
-    fun sendTokenToApi(){
+    fun doFcmTokenRegistration(){
         FirebaseMessaging.getInstance().token
             .addOnCompleteListener(OnCompleteListener { task ->
                 if (!task.isSuccessful) {
@@ -34,40 +34,10 @@ class MainActivity : AppCompatActivity() {
                     .permitAll().build()
                 StrictMode.setThreadPolicy(policy)
                 try {
-                    sendApiRequest(token)
+                    ApiInteraction.sendTokenToApi(token)
                 } catch (e: Exception) {
                     // update server status here
                 }
             })
     }
-
-    fun sendApiRequest(token: String) {
-        val mURL = URL("https://192.168.1.107:8000/registration/")
-        var reqParam = "{ \"token\": \"$token\"}"
-
-        with((mURL.openConnection() as HttpsURLConnection).apply{
-            sslSocketFactory = createSocketFactory(listOf("TLSv1.2"))
-            hostnameVerifier = HostnameVerifier { _, _ -> true }
-            readTimeout = 2_000
-            connectTimeout = 1_000
-        }){
-            // optional default is GET
-            requestMethod = "POST"
-
-            val wr = OutputStreamWriter(getOutputStream());
-            wr.write(reqParam);
-            wr.flush();
-        }
-    }
-
-    // trusts every cert as we're self-signing the django https server
-    private fun createSocketFactory(protocols: List<String>) =
-        SSLContext.getInstance(protocols[0]).apply {
-            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-                override fun checkClientTrusted(certs: Array<X509Certificate>, authType: String) = Unit
-                override fun checkServerTrusted(certs: Array<X509Certificate>, authType: String) = Unit
-            })
-            init(null, trustAllCerts, SecureRandom())
-        }.socketFactory
 }
